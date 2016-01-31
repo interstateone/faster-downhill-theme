b3770e58336ff7dfe7341a15579ad7da1cbf2bc0
Brandon Evans <brandon@brandonevans.ca>
2016-01-31 12:00:45 -0700

    Load latest location from tracker server

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
-//= require_tree .
+function getLatestLocation() {
+    $.ajax({
+        url: "https://tracker.brandonevans.ca/points/latest",
+        dataType: "jsonp",
+        success: function(point) {
+            var prefix = point.inside ? " in " : " near ";
+            var location = point.name;
+            $("h6 small").html(prefix + location);
+        }
+    });
+}
+
